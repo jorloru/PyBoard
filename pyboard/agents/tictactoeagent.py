6d31c3b118b3463956e88bd09911fdb07b61f736
jorloru <58566138+jorloru@users.noreply.github.com>
2023-02-24 19:34:14 +0100

    Added difficulties to TicTacToeAgent
    Added the fllowing difficulties to TicTacToeAgent:
    
    Easy: If there's a winning move, it goes for it.
    
    Medium: Same as easy, but if the center tile is empty it goes for it 100% of the time.
    
    Hard: Same as medium, but if it the opponent is winning the next turn, it tries to block it.

--- pyboard/agents/tictactoeagent.py
+++ pyboard/agents/tictactoeagent.py
@@ -10,7 +10,7 @@ import os
 sys.path.append(os.path.join('..','..'))
 
 import random
-from pyboard import TurnBasedAgent
+from pyboard import TurnBasedAgent, SetValueMove
 
 __all__ = 'TicTacToeAgent',
 
@@ -20,25 +20,146 @@ class TicTacToeAgent(TurnBasedAgent):
         
         super().__init__(player, difficulty)
         
-        if difficulty > 0:
+        if difficulty == 4:
             
-            print('Difficulty not yet implemented! Difficulty changed to random.')
-            self._Difficulty = 0
+            print('Difficulty not yet implemented! Difficulty changed to hard.')
+            self._Difficulty = 3
+            
+    def _get_wanning_moves(self, game_board, valid_moves, player):    
+        
+        moves = []
+        board = game_board.Board
+        
+        for i in range(game_board.Config.Rows):
+            for j in range(game_board.Config.Columns):
+                
+                # Dismiss move if tile is occupied already
+                if board[i,j] != 0:
+                    
+                    continue
+                
+                # Auxiliaries for packing cases
+                i_indexes = [x for x in range(3) if x!=i]
+                if j == 2: i_indexes.reverse()
+                
+                j_indexes = [x for x in range(3) if x!=j]
+                if i == 2: j_indexes.reverse()
+                
+                # Check verticals
+                if board[i,j_indexes[0]] == player and board[i,j_indexes[1]] == player:
+                    
+                    moves.append(SetValueMove(i, j, self._Player))
+                
+                # Check horizantals
+                if board[i_indexes[0],j] == player and board[i_indexes[1],j] == player:
+                    
+                    moves.append(SetValueMove(i, j, self._Player))
+                
+                # Check diagonals in extremes
+                if i in (0,2) and j in (0,2):
+                    
+                    if board[i_indexes[0],j_indexes[0]] == player and board[i_indexes[1],j_indexes[1]] == player:
+                        
+                        moves.append(SetValueMove(i, j, self._Player))
+                        
+                # Check diagonals in center
+                if i == 1 and j == 1:
+                    
+                    if (board[0,0] == player and board[2,2] == player) or (board[2,0] == player and board[0,2] == player):
+                        
+                        moves.append(SetValueMove(i, j, self._Player))
+                        
+        return moves
+    
+    def _get_winning_moves(self, game_board, valid_moves, player):    
+        
+        moves = []
+        board = game_board.Board
+        
+        for move in valid_moves:
+            
+            i, j = move.Row, move.Column
+            
+            # Auxiliaries for packing cases
+            i_indexes = [x for x in range(3) if x!=i]
+            if j == 2: i_indexes.reverse()
+            
+            j_indexes = [x for x in range(3) if x!=j]
+            if i == 2: j_indexes.reverse()
+            
+            # Check verticals
+            if board[i,j_indexes[0]] == player and board[i,j_indexes[1]] == player:
+                
+                moves.append(move)
+            
+            # Check horizantals
+            if board[i_indexes[0],j] == player and board[i_indexes[1],j] == player:
+                
+                moves.append(move)
+            
+            # Check diagonals in extremes
+            if i in (0,2) and j in (0,2):
+                
+                if board[i_indexes[0],j_indexes[0]] == player and board[i_indexes[1],j_indexes[1]] == player:
+                    
+                    moves.append(move)
+                    
+            # Check diagonals in center
+            if i == 1 and j == 1:
+                
+                if (board[0,0] == player and board[2,2] == player) or (board[2,0] == player and board[0,2] == player):
+                    
+                    moves.append(move)
+                        
+        return moves
     
     def _select_move_easy(self, game_board, valid_moves):
         
-        # TODO
-        pass
+        winning_moves = self._get_winning_moves(game_board, valid_moves, self._Player)
+        
+        if len(winning_moves) != 0:
+            
+            return random.choice(winning_moves)
+        
+        else:
+            
+            return random.choice(valid_moves)
     
     def _select_move_medium(self, game_board, valid_moves):
         
-        # TODO
-        pass
+        if game_board.Board[1,1] == 0:
+            
+            return SetValueMove(1, 1, self._Player)
+        
+        else:
+            
+            return self._select_move_easy(game_board, valid_moves)
     
     def _select_move_hard(self, game_board, valid_moves):
         
-        # TODO
-        pass
+        if game_board.Board[1,1] == 0:
+            
+            return SetValueMove(1, 1, self._Player)
+        
+        else:
+        
+            winning_moves = self._get_winning_moves(game_board, valid_moves, self._Player)
+            
+            if len(winning_moves) != 0:
+                
+                return random.choice(winning_moves)
+            
+            else:
+                
+                losing_moves = self._get_winning_moves(game_board, valid_moves, self._Player%2+1)
+                
+                if len(losing_moves) != 0:
+                    
+                    return random.choice(losing_moves)
+                
+                else:
+                    
+                    return random.choice(valid_moves)
     
     def _select_move_extreme(self, game_board, valid_moves):
         
